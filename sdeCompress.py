1c1f88c46f96280dd22c0cd75e19bd870fe40568
jcguarneri <jcguarneri@gmail.com>
2017-09-20 08:53:46 -0400

    Code updates
    Simplified index and analyze steps. Added clearer comment lines.

--- sdeCompress.py
+++ sdeCompress.py
@@ -1,48 +1,38 @@
 import arcpy
 
-# Process: Compress
+#compress the geodatabase
 arcpy.Compress_management("C:\Users\GeoNexusAdmin\AppData\Roaming\ESRI\Desktop10.4\ArcCatalog\Connection to asteroid.sde")
 
 try:
     
+    #collect information on compress messages for log file
     cMessage = arcpy.GetMessages()
     cMessageCount = arcpy.GetMessageCount()
        
+    #open log file and write compress message information
     logFile = open(r'C:\Users\GeoNexusAdmin\Desktop\compressLog.txt', 'wb')
     logFile.write('End Message:%s\nNumber of messages: %s'%(cMessage,cMessageCount)
                   
                   
     # set the workspace 
     arcpy.env.workspace = r"C:\Users\GeoNexusAdmin\AppData\Roaming\ESRI\Desktop10.4\ArcCatalog\Connection to asteroid.sde"
+    workspace = arcpy.env.workspace
+                  
+    # Rebuild indexes and analyze the states and states_lineages system tables
+    arcpy.RebuildIndexes_management(workspace, "SYSTEM", "ALL")
 
-    # Get the user name for the workspace
-    # this assumes you are using database authentication.
-    # OS authentication connection files do not have a 'user' property.
-    userName = arcpy.Describe(arcpy.env.workspace).connectionProperties.user
-
-    # Get a list of all the datasets the user has access to.
-    # First, get all the stand alone tables, feature classes and rasters owned by the current user.
-    dataList = arcpy.ListTables('*.' + userName + '.*') + arcpy.ListFeatureClasses('*.' + userName + '.*') + arcpy.ListRasters('*.' + userName + '.*')
-
-    # Next, for feature datasets owned by the current user
-    # get all of the featureclasses and add them to the master list.
-    for dataset in arcpy.ListDatasets('*.' + userName + '.*'):
-        dataList += arcpy.ListFeatureClasses(feature_dataset=dataset)
-
-    # Execute rebuild indexes and analyze datasets
-    # Note: to use the "SYSTEM" option, the user must be an administrator.
-
-    workspace = r"C:\Users\GeoNexusAdmin\AppData\Roaming\ESRI\Desktop10.4\ArcCatalog\Connection to asteroid.sde"
-
-    arcpy.RebuildIndexes_management(workspace, "NO_SYSTEM", dataList, "ALL")
-
-    arcpy.AnalyzeDatasets_management(workspace, "NO_SYSTEM", dataList, "ANALYZE_BASE", "ANALYZE_DELTA", "ANALYZE_ARCHIVE")
+    arcpy.AnalyzeDatasets_management(workspace, "SYSTEM", "ANALYZE_BASE", "ANALYZE_DELTA", "ANALYZE_ARCHIVE")
     
+    #write success confirmation to log file and save              
     logFile.write("\nSuccess!")
     logFile.close()             
                   
 except:
+    
+    #collect messages from last run tool              
     messageText = arcpy.GetMessages()
+    
+    #open log file and write the messages and failure notice
     logFile = open(r'C:\Users\GeoNexusAdmin\Desktop\compressLog.txt', 'wb')
     logFile.write("Index failed")
     logFile.write(messageText)
